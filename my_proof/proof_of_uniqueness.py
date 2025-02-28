c154c8d9c45a3b6ca9b7937a062e9b4292464744
Ryzer-X <devs@zlayer.ai>
2025-02-28 16:41:55 +0530

    feat: prod version

--- my_proof/proof_of_uniqueness.py
+++ my_proof/proof_of_uniqueness.py
@@ -33,21 +33,21 @@ def get_redis_client():
 # Fetch file mappings from API
 # TODO: Remove comments
 def get_file_mappings(wallet_address):
-    # validator_base_api_url = os.environ.get('VALIDATOR_BASE_API_URL')
-    # endpoint = "/api/userinfo"
-    # url = f"{validator_base_api_url.rstrip('/')}{endpoint}"
+    validator_base_api_url = os.environ.get('VALIDATOR_BASE_API_URL')
+    endpoint = "/api/userinfo"
+    url = f"{validator_base_api_url.rstrip('/')}{endpoint}"
 
-    # payload = {"walletAddress": wallet_address}  # Send walletAddress in the body
-    # headers = {"Content-Type": "application/json"}  # Set headers for JSON request
+    payload = {"walletAddress": wallet_address}  # Send walletAddress in the body
+    headers = {"Content-Type": "application/json"}  # Set headers for JSON request
 
-    # response = requests.post(url, json=payload, headers=headers)  # Make POST request
+    response = requests.post(url, json=payload, headers=headers)  # Make POST request
 
-    # if response.status_code == 200:
-    #     return response.json()  # Return JSON response
-    # else:
-        # return []  # Return empty list in case of an error
-    return [{"fileId":119, "fileUrl":"https://drive.google.com/uc?export=download&id=1niGj_CmVap_rJotn-BQ3YrF7AxNIJBED"},]
-            # {"fileId":1607848, "fileUrl":"https://drive.google.com/uc?export=download&id=16xQSjQ1KGNwSJZTA84Ex2v6Z2IGAEDyo"}]
+    if response.status_code == 200:
+        return response.json()  # Return JSON response
+    else:
+        return []  # Return empty list in case of an error
+    # return [{"fileId":119, "fileUrl":"https://drive.google.com/uc?export=download&id=1niGj_CmVap_rJotn-BQ3YrF7AxNIJBED"},]
+    #         # {"fileId":1607848, "fileUrl":"https://drive.google.com/uc?export=download&id=16xQSjQ1KGNwSJZTA84Ex2v6Z2IGAEDyo"}]
 
 # Download and decrypt file
 def download_and_decrypt(file_url, gpg_signature):
